a73d54c10938f423244077ceb905950d510e7490
Chimboys <akmal@usf.edu>
2024-06-26 13:01:09 -0400

    Added User Class

--- first_example.py
+++ first_example.py
@@ -12,4 +12,16 @@ print("The same task but with lambda and list comprehension")
 add = lambda x: print(f"{x} is even") if x % 2 == 0 else print(f"{x} is odd")
 [add(i) for i in range(1, 11)]
 
+print("Welcome To GitHub")
+class User:
+    def __init__(self,name):
+        self.name = name
+    def print_name(self):
+        print(f"My name is {self.name}")
+
+Akmal = User("Akmal")
+Akmal.print_name()
+    
+
+
     
