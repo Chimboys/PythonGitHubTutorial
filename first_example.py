a70a7e192a2126f914bd12973391c4161d0e40c5
Chimboys <akmal@usf.edu>
2024-06-26 12:21:42 -0400

    Added list comprehension and improved for loop

--- first_example.py
+++ first_example.py
@@ -1,6 +1,15 @@
-print("Time to learn git!")
+#Akmal Kurbanov
+#Learning Git
 
+print("Time to learn git!")
 for i in range(1,11):
     if (i % 2) == 0:
-        print(i)
+        print(i , "is even")
+    else:
+        print(i, "is odd")
+
+print("The same task but with lambda and list comprehension")
+add = lambda x: print(f"{x} is even") if x % 2 == 0 else print(f"{x} is odd")
+[add(i) for i in range(1, 11)]
+
     
